fbcb65df96da8aab868aa2e7fcd367fe0cebfd40
Tim Graham <timograham@gmail.com>
2024-05-29 11:26:54 -0400

    Added DatabaseFeatures.supports_microsecond_precision.
    This reverts commit a80903b7114c984b5087597e8c34750e7bb44f51.

--- tests/timezones/tests.py
+++ tests/timezones/tests.py
@@ -100,11 +100,20 @@ class LegacyDatabaseTests(TestCase):
         event = Event.objects.get()
         self.assertEqual(event.dt, dt)
 
+    @skipUnlessDBFeature("supports_microsecond_precision")
     def test_naive_datetime_with_microsecond(self):
         dt = datetime.datetime(2011, 9, 1, 13, 20, 30, 405060)
         Event.objects.create(dt=dt)
         event = Event.objects.get()
         self.assertEqual(event.dt, dt)
+
+    @skipIfDBFeature("supports_microsecond_precision")
+    def test_naive_datetime_with_microsecond_unsupported(self):
+        dt = datetime.datetime(2011, 9, 1, 13, 20, 30, 405060)
+        Event.objects.create(dt=dt)
+        event = Event.objects.get()
+        # microseconds are lost during a round-trip in the database
+        self.assertEqual(event.dt, dt.replace(microsecond=405000))
 
     @skipUnlessDBFeature("supports_timezones")
     def test_aware_datetime_in_local_timezone(self):
@@ -116,6 +125,7 @@ class LegacyDatabaseTests(TestCase):
         self.assertEqual(event.dt.replace(tzinfo=EAT), dt)
 
     @skipUnlessDBFeature("supports_timezones")
+    @skipUnlessDBFeature("supports_microsecond_precision")
     def test_aware_datetime_in_local_timezone_with_microsecond(self):
         dt = datetime.datetime(2011, 9, 1, 13, 20, 30, 405060, tzinfo=EAT)
         Event.objects.create(dt=dt)
@@ -123,6 +133,18 @@ class LegacyDatabaseTests(TestCase):
         self.assertIsNone(event.dt.tzinfo)
         # interpret the naive datetime in local time to get the correct value
         self.assertEqual(event.dt.replace(tzinfo=EAT), dt)
+
+    # This combination actually never happens.
+    @skipUnlessDBFeature("supports_timezones")
+    @skipIfDBFeature("supports_microsecond_precision")
+    def test_aware_datetime_in_local_timezone_with_microsecond_unsupported(self):
+        dt = datetime.datetime(2011, 9, 1, 13, 20, 30, 405060, tzinfo=EAT)
+        Event.objects.create(dt=dt)
+        event = Event.objects.get()
+        self.assertIsNone(event.dt.tzinfo)
+        # interpret the naive datetime in local time to get the correct value
+        # microseconds are lost during a round-trip in the database
+        self.assertEqual(event.dt.replace(tzinfo=EAT), dt.replace(microsecond=0))
 
     @skipUnlessDBFeature("supports_timezones")
     def test_aware_datetime_in_utc(self):
@@ -338,6 +360,7 @@ class NewDatabaseTests(TestCase):
             Event.objects.annotate(unbound_datetime=Now()).filter(unbound_datetime=dt)
 
     @requires_tz_support
+    @skipUnlessDBFeature("supports_microsecond_precision")
     def test_naive_datetime_with_microsecond(self):
         dt = datetime.datetime(2011, 9, 1, 13, 20, 30, 405060)
         with self.assertWarnsMessage(RuntimeWarning, self.naive_warning):
@@ -346,17 +369,37 @@ class NewDatabaseTests(TestCase):
         # naive datetimes are interpreted in local time
         self.assertEqual(event.dt, dt.replace(tzinfo=EAT))
 
+    @requires_tz_support
+    @skipIfDBFeature("supports_microsecond_precision")
+    def test_naive_datetime_with_microsecond_unsupported(self):
+        dt = datetime.datetime(2011, 9, 1, 13, 20, 30, 405060)
+        with self.assertWarnsMessage(RuntimeWarning, self.naive_warning):
+            Event.objects.create(dt=dt)
+        event = Event.objects.get()
+        # microseconds are lost during a round-trip in the database
+        # naive datetimes are interpreted in local time
+        self.assertEqual(event.dt, dt.replace(microsecond=405000, tzinfo=EAT))
+
     def test_aware_datetime_in_local_timezone(self):
         dt = datetime.datetime(2011, 9, 1, 13, 20, 30, tzinfo=EAT)
         Event.objects.create(dt=dt)
         event = Event.objects.get()
         self.assertEqual(event.dt, dt)
 
+    @skipUnlessDBFeature("supports_microsecond_precision")
     def test_aware_datetime_in_local_timezone_with_microsecond(self):
         dt = datetime.datetime(2011, 9, 1, 13, 20, 30, 405060, tzinfo=EAT)
         Event.objects.create(dt=dt)
         event = Event.objects.get()
         self.assertEqual(event.dt, dt)
+
+    @skipIfDBFeature("supports_microsecond_precision")
+    def test_aware_datetime_in_local_timezone_with_microsecond_unsupported(self):
+        dt = datetime.datetime(2011, 9, 1, 13, 20, 30, 405060, tzinfo=EAT)
+        Event.objects.create(dt=dt)
+        event = Event.objects.get()
+        # microseconds are lost during a round-trip in the database
+        self.assertEqual(event.dt, dt.replace(microsecond=405000))
 
     def test_aware_datetime_in_utc(self):
         dt = datetime.datetime(2011, 9, 1, 10, 20, 30, tzinfo=UTC)
@@ -1398,6 +1441,7 @@ class AdminTests(TestCase):
     @requires_tz_support
     def test_change_readonly(self):
         t = Timestamp.objects.create()
+        t.refresh_from_db()  # milliseconds may have been truncated
         response = self.client.get(
             reverse("admin_tz:timezones_timestamp_change", args=(t.pk,))
         )
@@ -1405,6 +1449,7 @@ class AdminTests(TestCase):
 
     def test_change_readonly_in_other_timezone(self):
         t = Timestamp.objects.create()
+        t.refresh_from_db()  # milliseconds may have been truncated
         with timezone.override(ICT):
             response = self.client.get(
                 reverse("admin_tz:timezones_timestamp_change", args=(t.pk,))
