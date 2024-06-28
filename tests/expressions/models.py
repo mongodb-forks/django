f92439dfd94b3f4e59250815f7a0e1fec729218e
Tim Graham <timograham@gmail.com>
2024-06-28 10:27:28 -0400

    Fixed Number.__str__() crash when float/decimal_value is None in expressions tests models.

--- tests/expressions/models.py
+++ tests/expressions/models.py
@@ -58,9 +58,9 @@ class Number(models.Model):
     def __str__(self):
         return "%i, %s, %s" % (
             self.integer,
-           "%.3f" % self.float if self.float is not None else None,
-           "%.17f" % self.decimal_value if self.decimal_value is not None else None,
-       )
+            "%.3f" % self.float if self.float is not None else None,
+            "%.17f" % self.decimal_value if self.decimal_value is not None else None,
+        )
 
 
 class Experiment(models.Model):
