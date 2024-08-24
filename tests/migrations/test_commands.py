0a66b480fd6a8350551eac2e542eb419d9f98e2b
Tim Graham <timograham@gmail.com>
2024-08-23 20:25:07 -0400

    schema and migrations test edits

--- tests/migrations/test_commands.py
+++ tests/migrations/test_commands.py
@@ -907,10 +907,7 @@ class MigrateTests(MigrationTestBase):
                 "--",
             ],
         )
-        self.assertIn(
-            "create table %s" % connection.ops.quote_name("migrations_author").lower(),
-            lines[3].lower(),
-        )
+        self.assertIn("db.create_collection('migrations_author')", lines[3])
         pos = lines.index("--", 3)
         self.assertEqual(
             lines[pos : pos + 3],
@@ -920,10 +917,7 @@ class MigrateTests(MigrationTestBase):
                 "--",
             ],
         )
-        self.assertIn(
-            "create table %s" % connection.ops.quote_name("migrations_tribble").lower(),
-            lines[pos + 3].lower(),
-        )
+        self.assertIn("db.create_collection('migrations_tribble')", lines[pos + 3])
         pos = lines.index("--", pos + 3)
         self.assertEqual(
             lines[pos : pos + 3],
@@ -932,6 +926,10 @@ class MigrateTests(MigrationTestBase):
                 "-- Add field bool to tribble",
                 "--",
             ],
+        )
+        self.assertEqual(
+            "db.migrations_tribble.update_many({}, [{'$set': {'bool': False}}])",
+            lines[pos + 3],
         )
         pos = lines.index("--", pos + 3)
         self.assertEqual(
@@ -942,6 +940,7 @@ class MigrateTests(MigrationTestBase):
                 "--",
             ],
         )
+        self.assertIn("db.migrations_author.create_indexes([", lines[pos + 3])
 
     @override_settings(MIGRATION_MODULES={"migrations": "migrations.test_migrations"})
     def test_sqlmigrate_backwards(self):
@@ -962,6 +961,7 @@ class MigrateTests(MigrationTestBase):
         )
 
         lines = out.getvalue().splitlines()
+
         try:
             if connection.features.can_rollback_ddl:
                 self.assertEqual(lines[0], connection.ops.start_transaction_sql())
@@ -976,6 +976,11 @@ class MigrateTests(MigrationTestBase):
                     "--",
                 ],
             )
+            self.assertEqual(
+                "db.migrations_author.drop_index"
+                "('migrations_author_name_slug_0ef2ba54_uniq')",
+                lines[3],
+            )
             pos = lines.index("--", 3)
             self.assertEqual(
                 lines[pos : pos + 3],
@@ -984,6 +989,10 @@ class MigrateTests(MigrationTestBase):
                     "-- Add field bool to tribble",
                     "--",
                 ],
+            )
+            self.assertEqual(
+                "db.migrations_tribble.update_many({}, {'$unset': {'bool': ''}})",
+                lines[pos + 3],
             )
             pos = lines.index("--", pos + 3)
             self.assertEqual(
@@ -995,10 +1004,7 @@ class MigrateTests(MigrationTestBase):
                 ],
             )
             next_pos = lines.index("--", pos + 3)
-            drop_table_sql = (
-                "drop table %s"
-                % connection.ops.quote_name("migrations_tribble").lower()
-            )
+            drop_table_sql = "db.migrations_tribble.drop()"
             for line in lines[pos + 3 : next_pos]:
                 if drop_table_sql in line.lower():
                     break
@@ -1013,9 +1019,7 @@ class MigrateTests(MigrationTestBase):
                     "--",
                 ],
             )
-            drop_table_sql = (
-                "drop table %s" % connection.ops.quote_name("migrations_author").lower()
-            )
+            drop_table_sql = "db.migrations_author.drop()"
             for line in lines[pos + 3 :]:
                 if drop_table_sql in line.lower():
                     break
