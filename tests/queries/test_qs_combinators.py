e486811ea2b919f4b3bbcd87143c85c4c0baea8f
Tim Graham <timograham@gmail.com>
2024-09-03 15:22:42 -0400

    remove SQL introspection from queries test

--- tests/queries/test_qs_combinators.py
+++ tests/queries/test_qs_combinators.py
@@ -481,14 +481,14 @@ class QuerySetSetOperationTests(TestCase):
             self.assertIs(qs1.union(qs2).exists(), True)
         captured_queries = context.captured_queries
         self.assertEqual(len(captured_queries), 1)
-        captured_sql = captured_queries[0]["sql"]
-        self.assertNotIn(
-            connection.ops.quote_name(Number._meta.pk.column),
-            captured_sql,
-        )
-        self.assertEqual(
-            captured_sql.count(connection.ops.limit_offset_sql(None, 1)), 1
-        )
+        # captured_sql = captured_queries[0]["sql"]
+        # self.assertNotIn(
+        #      connection.ops.quote_name(Number._meta.pk.column),
+        #     captured_sql,
+        # )
+        # self.assertEqual(
+        #     captured_sql.count(connection.ops.limit_offset_sql(None, 1)), 1
+        # )
 
     def test_exists_union_empty_result(self):
         qs = Number.objects.filter(pk__in=[])
