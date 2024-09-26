5e0a394d252f5d9d1a788f569f72e77b7c4456e6
Tim Graham <timograham@gmail.com>
2024-09-25 20:08:15 -0400

    edits for many test apps

--- tests/serializers/test_json.py
+++ tests/serializers/test_json.py
@@ -29,14 +29,14 @@ class JsonSerializerTestCase(SerializersTestBase, TestCase):
     mapping_ordering_str = """[
 {
   "model": "serializers.article",
-  "pk": %(article_pk)s,
+  "pk": "%(article_pk)s",
   "fields": {
-    "author": %(author_pk)s,
+    "author": "%(author_pk)s",
     "headline": "Poker has no place on ESPN",
     "pub_date": "2006-06-16T11:00:00",
     "categories": [
-      %(first_category_pk)s,
-      %(second_category_pk)s
+      "%(first_category_pk)s",
+      "%(second_category_pk)s"
     ],
     "meta_data": [],
     "topics": []
