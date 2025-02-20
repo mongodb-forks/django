43e6e815d08aef864d94ca6eeec64fbdbb55d474
Tim Graham <timograham@gmail.com>
2025-02-20 11:19:36 -0500

    Fixed #36201 -- Fixed `ModelChoiceField/ModelMultipleChoiceField.clean()` to catch `ValidationError` from queryset operations.

--- django/forms/models.py
+++ django/forms/models.py
@@ -1555,7 +1555,12 @@ class ModelChoiceField(ChoiceField):
             if isinstance(value, self.queryset.model):
                 value = getattr(value, key)
             value = self.queryset.get(**{key: value})
-        except (ValueError, TypeError, self.queryset.model.DoesNotExist):
+        except (
+            ValueError,
+            TypeError,
+            ValidationError,
+            self.queryset.model.DoesNotExist,
+        ):
             raise ValidationError(
                 self.error_messages["invalid_choice"],
                 code="invalid_choice",
@@ -1632,7 +1637,7 @@ class ModelMultipleChoiceField(ModelChoiceField):
         for pk in value:
             try:
                 self.queryset.filter(**{key: pk})
-            except (ValueError, TypeError):
+            except (ValueError, TypeError, ValidationError):
                 raise ValidationError(
                     self.error_messages["invalid_pk_value"],
                     code="invalid_pk_value",
